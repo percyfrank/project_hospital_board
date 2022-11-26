d9b28a070aae0138e2827bee8c1cdc73caa1e0b9
ohsuk kwon <percykwon@gmail.com>
2022-11-26 21:35:41 +0900

    UPDATE : 특정 병원의 리뷰만 조회하기 위한 메서드 추가

--- src/main/java/com/springboot/relationship/repository/ReviewRepository.java
+++ src/main/java/com/springboot/relationship/repository/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.springboot.relationship.repository;
 
+import com.springboot.relationship.domain.entity.Hospital;
 import com.springboot.relationship.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review,Integer> {
+    List<Review> findByHospital(Hospital hospital);
 }
